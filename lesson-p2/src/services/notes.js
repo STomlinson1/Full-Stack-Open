d8fe7f5ae82954df84c090961eaf51c388fab6b2
Stephon Tomlinson <stephon.tomlinson@gmail.com>
2020-09-04 12:45:03 -0400

    remove test

--- lesson-p2/src/services/notes.js
+++ lesson-p2/src/services/notes.js
@@ -3,13 +3,7 @@ const baseUrl = '/api/notes';
 
 const getAll = () => {
 	const request = axios.get(baseUrl);
-	const nonExisting = {
-		id        : 100000,
-		content   : 'this note is not saved to the server',
-		data      : '2019-05-30T17:30:31.098Z',
-		important : true
-	};
-	return request.then((response) => response.data.concat(nonExisting));
+	return request.then((response) => response.data);
 };
 
 const create = (newObject) => {
